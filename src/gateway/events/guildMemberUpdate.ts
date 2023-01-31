8a5467bc803f88010fedaae5a4504f013928f975
Snow <73443391+JustAWaifuHunter@users.noreply.github.com>
2023-01-31 00:46:12 -0300

    refactor: ops

--- src/gateway/events/guildMemberUpdate.ts
+++ src/gateway/events/guildMemberUpdate.ts
@@ -13,7 +13,6 @@ export class GuildMemberUpdate extends Event {
     if (!guild) return;
 
     const oldMember = structuredClone(guild.members.get(data.user.id));
-    console.log(oldMember)
     const updatedData = oldMember
       ? (Object.assign(oldMember.partial, data) as APIGuildMember)
       : { ...data, deaf: null, mute: null };
