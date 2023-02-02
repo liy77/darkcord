14b818110553b56179e85d8776e5afa5aa96ebca
Snow <73443391+JustAWaifuHunter@users.noreply.github.com>
2023-02-02 02:20:06 -0300

    refactor: :hammer: move Rest Error and Discord API Error to errors file

--- src/utils/Errors.ts
+++ src/utils/Errors.ts
@@ -1,6 +1,52 @@
 import { MakeError } from "./index";
 
-export const MissingIntentsError = (...intents: string[]) => MakeError({
+export const MissingIntentsError = (...intents: string[]) =>
+  MakeError({
     name: "MissingIntents",
-    message: `Intents ${intents.join(", ")} is missing in Client`
-})
+    message: `Intents ${intents.join(", ")} is missing in Client`,
+  });
+
+export const InvalidTokenError = MakeError({
+  name: "InvalidToken",
+  message: "Invalid token was provided",
+});
+
+export class DiscordAPIError extends Error {
+  constructor(
+    public router: string,
+    public method: string,
+    public code: number,
+    public status: number,
+    errors: Record<string, unknown>
+  ) {
+    super();
+
+    this.name = "DiscordAPIError";
+    if (Array.isArray(errors)) {
+      errors.push({ code, method, router });
+
+      this.message = JSON.stringify(errors, null, 4);
+    } else {
+      errors.code = code;
+      errors.method = method;
+      errors.router = router;
+
+      if ("message" in errors) {
+        this.message = errors.message as string;
+      }
+    }
+  }
+}
+
+export class RequestError extends Error {
+  constructor(
+    public router: string,
+    public method: string,
+    public message: string,
+    public name: string,
+    public code: number
+  ) {
+    super();
+    this.name = "RequestError";
+  }
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -37,40 +37,6 @@ export function delay(time: number) {
   return new Promise((resolve) => setTimeout(resolve, time).unref());
 }
 
-export class DiscordAPIError extends Error {
-  constructor(
-    public router: string,
-    public method: string,
-    public code: number,
-    errors: Record<string, unknown>
-  ) {
-    super();
-
-    this.name = "DiscordAPIError";
-    if (Array.isArray(errors)) {
-      errors.push({ code, method, router });
-    } else {
-      errors.code = code;
-      errors.method = method;
-      errors.router = router;
-    }
-    this.message = JSON.stringify(errors, null, 4);
-  }
-}
-
-export class RequestError extends Error {
-  constructor(
-    public router: string,
-    public method: string,
-    public message: string,
-    public name: string,
-    public code: number
-  ) {
-    super();
-    this.name = "RequestError";
-  }
-}
-
 export function MakeError(
   options: MakeErrorOptions,
   type:
