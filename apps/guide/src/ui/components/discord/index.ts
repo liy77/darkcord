0c635a8f2a4fab6cf24b1658f199a5a3fe27cf45
Nicolas Ribeiro <commit@niskii.dev>
2023-02-03 15:56:43 -0300

    fix imports (again)

--- apps/guide/src/ui/components/discord/index.ts
+++
@@ -1,9 +0,0 @@
-// by https://github.com/discordjs/discord.js/tree/main/packages/ui
-
-export * from './Message.jsx';
-export * from './MessageAuthor.jsx';
-export * from './MessageAuthorReply.jsx';
-export * from './MessageBaseReply.jsx';
-export * from './MessageInteraction.jsx';
-export * from './MessageReply.jsx';
-export * from './Messages.jsx';
