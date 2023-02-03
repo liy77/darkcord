0eeb180294bba6a116934899488a786fd781044a
Nicolas Ribeiro <commit@niskii.dev>
2023-02-03 16:54:18 -0300

    fix(guide): vercel issues (#6)
    * remove Discord folder
    
    * add folder again
    
    * remove search component
    
    * add search component again

---
+++ apps/guide/src/ui/components/Discord/index.ts
@@ -0,0 +1,8 @@
+export * from './Message';
+export * from './MessageAuthor';
+export * from './MessageAuthorReply';
+export * from './MessageBaseReply';
+export * from './MessageInteraction';
+export * from './MessageReply';
+export * from './Messages';
+
