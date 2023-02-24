4831fb47d45979546e070619c623fad488a8e4d3
Nicolas Ribeiro <commit@niskii.dev>
2023-02-24 00:04:49 -0300

    use api to extract docs data

--- apps/website/src/app/docAPI.ts
+++ apps/website/src/app/docAPI.ts
@@ -1,23 +1,17 @@
-import { readFile } from "node:fs/promises";
-import { join } from "node:path";
 
 export async function fetchModelJSON() {
+  // const res = await readFile(
+  //   join(process.cwd(), "public", "docs.api.json"),
+  //   "utf8",
+  // );
 
-    const res = await readFile(
-      join(
-        process.cwd(),
-        "public",
-        "docs.api.json",
-      ),
-      "utf8",
-    );
+  // return JSON.parse(res);
 
-    return JSON.parse(res);
-  
+  const res = await fetch("https://pics.niskii.dev/docs.api.json", {
+    next: { revalidate: 60 * 60 * 24 },
+  });
 
-  // const res = await fetch("https://pics.niskii.dev/docs.api.json", {
-  //   next: { revalidate: 3_600 },
-  // });
+  const data = await res.json()
 
-  // return res.json();
+  return data
 }
