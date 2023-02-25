14a257424f45a850f31a6adde61e96dd9d5fdf28
JustAWaifuHunter <hcsbrian4056@gmail.com>
2023-02-25 18:33:14 -0300

    dasd

--- apps/website/src/app/docs/page.tsx
+++ apps/website/src/app/docs/page.tsx
@@ -43,7 +43,7 @@ export default async function Page() {
   const mdxSource = await generateMDX(readmeSource);
 
   return (
-    <article className="dark:bg-dark-800 bg-white p-6 pb-20 shadow">
+    <article className="position fixed dark:bg-dark-800 bg-white p-6 pb-20 shadow">
       <div className="prose max-w-none">
         <MDXRemote {...mdxSource} />
       </div>
