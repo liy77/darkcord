c7eccca49a968f0756ec961910fb69b0d7b0c705
JustAWaifuHunter <hcsbrian4056@gmail.com>
2023-02-26 02:57:14 -0300

    test 5

--- apps/website/src/app/docs/page.tsx
+++ apps/website/src/app/docs/page.tsx
@@ -43,7 +43,7 @@ export default async function Page() {
   const mdxSource = await generateMDX(readmeSource);
 
   return (
-    <article className="relative margin-left 76em float left dark:bg-dark-800 bg-white p-6 pb-20 shadow">
+    <article className="margin-left 76em float right dark:bg-dark-800 bg-white p-6 pb-20 shadow">
       <div className="prose max-w-none">
         <MDXRemote {...mdxSource} />
       </div>
