f65c815b3d2791de547f5a055d5a6026bcb37807
Nicolas Ribeiro <nicolas.ribeiro.s@outlook.com>
2024-02-03 23:13:44 -0300

    chore: remove website (#17)

--- apps/guide/src/ui/components/Discord/index.ts
+++
@@ -1,9 +0,0 @@
-// fork of https://github.com/discordjs/discord.js/tree/main/packages/ui/src/lib/components/discord
-
-export * from './Message';
-export * from './MessageAuthor';
-export * from './MessageAuthorReply';
-export * from './MessageBaseReply';
-export * from './MessageInteraction';
-export * from './MessageReply';
-export * from './Messages';
