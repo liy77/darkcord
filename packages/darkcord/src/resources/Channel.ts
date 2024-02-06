93e223d4ac5cf8a2a31b43d340688b6ff8326890
Liy <hcsbrian4056@gmail.com>
2024-02-06 00:54:23 -0300

    fix: :bug: add missing props in edit channel

--- packages/darkcord/src/resources/Channel.ts
+++ packages/darkcord/src/resources/Channel.ts
@@ -99,6 +99,7 @@ export class Channel extends Base {
         default_sort_order: options.defaultSortOrder,
         default_thread_rate_limit_per_user:
           options.defaultThreadRateLimitPerUser,
+        default_forum_layout: options.defaultForumLayout,
         topic: options.topic,
         bitrate: options.bitrate,
         available_tags: options.availableTags,
@@ -107,6 +108,12 @@ export class Channel extends Base {
         rtc_region: options.rtcRegion,
         user_limit: options.userLimit,
         position: options.position,
+        permission_overwrites: options.permissionOverwrites,
+        applied_tags: options.appliedTags,
+        archived: options.archived,
+        parent_id: options.parentId,
+        rate_limit_per_user: options.rateLimitPerUser,
+        flags: options.flags,
       },
       reason,
     );
