160cbaa48bd9c2f4be0cdfd259abc778e63fe976
Liy <hcsbrian4056@gmail.com>
2024-02-14 02:51:24 -0300

    fix: typing

--- packages/darkcord/src/resources/Guild.ts
+++ packages/darkcord/src/resources/Guild.ts
@@ -913,7 +913,7 @@ export class Guild extends BaseGuild {
     let emojiImage: Base64File | undefined;
 
     if (options.data && options.data instanceof Buffer) {
-      emojiImage = "data:image/jpg;base64," + options.data.toString("base64");
+      emojiImage = `data:image/jpg;base64,${options.data.toString("base64")}` as Base64File;
     } else if (
       options.data &&
       typeof options.data === "string" &&
